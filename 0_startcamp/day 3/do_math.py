7ef9d5933ecc8bf3bfb34c7eba165d453f413168
ermes kwon <kwonzoidsbob@gmail.com>
2018-12-20 16:22:17 +0900

    so gross

---
+++ 0_startcamp/day 3/do_math.py
@@ -0,0 +1,15 @@
+print('!', __name__)
+# import math_functions
+# math_functions.cube(5)
+# math_functions.average([10, 20, 30])
+from math_functions import cube, average
+
+
+print(cube(5)) # 무조건 메소드로 만들자!
+print(average([10, 20, 30]))
+
+
+
+# 만약 from이 없고 import math_functions 를 한다면
+# print(math_functions.cube(5))
+# print(math_functions.average[10, 20, 30])) 를 하면 됨
--- 0_startcamp/day 3/math_functions.py
+++ 0_startcamp/day 3/math_functions.py
@@ -1,3 +1,5 @@
+print(__name__)
+
 def average(numbers): # 함수안에서 함수 갖다쓸 수 있다.
     return sum(numbers) / len(numbers) # result = sum(nubers) / len(numbers)
                                        # return result 이래도 된다
@@ -7,7 +9,12 @@ def cube(x):
 
 
 # 1. 평균값을 구하시오
-my_score = [79, 84, 66, 93]
-print(my_score)
-print(average(my_score))
-print(cube(3))
+
+def main(): # 이걸 해야 math_functions를 import한 곳에서 굳이 또 값 안나옴!
+    my_score = [79, 84, 66, 93]
+    print(average(my_score))
+    print(cube(3))
+
+
+if __name__ == '__main__':
+    main() #위의 거랑 같은 말.
