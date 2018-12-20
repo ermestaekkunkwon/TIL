aea798acd970330f56677552d179b872750044e6
ermes kwon <kwonzoidsbob@gmail.com>
2018-12-20 17:02:41 +0900

    hi

--- 0_startcamp/day 3/check_lotto.py
+++ 0_startcamp/day 3/check_lotto.py
@@ -1,5 +1,5 @@
 import lotto_functions
-# 혹은 from을 써라 -> result = am_i_lucky(pick_lotto(), get_lotto())
+# 혹은 from을 써라 -> result = am_i_lucky(pick_lotto(), get_lotto()) 이렇게만 써도 된다!
 
 
 result = lotto_functions.am_i_lucky(
