a8f2b55bd1101106e8bfc018c22199805c810dcc
ermes kwon <kwonzoidsbob@gmail.com>
2019-01-14 18:00:01 +0900

    asdf

---
+++ 0_startcamp/day7/maruta.py
@@ -0,0 +1,9 @@
+import keyword
+
+print (keyword.kwlist)
+
+
+a = 1
+A = 2
+print(a)
+print(A)
---
+++ 0_startcamp/day7/maruta2.py
@@ -0,0 +1,15 @@
+def ss3 (a, b):
+    """ 멀티스퀘어 703호
+    반장: 황은석
+    총무: 강진우
+    새해복 많이 받으세요
+    """
+    return a + b
+
+
+print(ss3 (1, 2))
+print(ss3.__doc__)
+
+
+
+print ('a') ; print('b')
